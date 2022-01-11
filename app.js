c6ddc9b17b30e7e08c957c33de6f557cc03be863
Choi Hannwi <lllhn@naver.com>
2022-01-11 18:52:20 +0900

    [22.01.11] # 프로젝트2일차

--- app.js
+++ app.js
@@ -1,36 +1,88 @@
+const $link = 'https://github.com';
+
+document.querySelector('.logo').addEventListener('click', e => {
+    window.open($link);
+    console.log('링크');
+});
+
+const URL = 'https://api.github.com';
 
 
-const URL = 'https://api.github.com/users';
-// 메인 로고 누르면 깃허브 홈페이지로 가는 함수
-function mainGithub () {
-    
-    const $link = 'https://github.com';
-    
-    document.querySelector('.logo').addEventListener('click', e => {
-        window.open($link);
-        console.log('링크');
+
+// 프로필 렌더링 함수
+function renderingProfile() {
+    let userId = document.getElementById('search').value;
+    const result = fetch(URL + '/users/' + `${userId}`, {
+        headers: {
+            Authorization: "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"
+        },
     });
-}
-
-function getUser() {
-
-    fetch(`https://api.github.com/users`)
-        .then(res => {
-            console.log(res);
-            if (res.status === 200) {
-                return res.json();
-            } else {
-                alert('통신 실패!');
-                return null;
-            }     
-        });
+    
+    result.then(res => res.json())
+    .then(user => {
         
+        // 사진
+        document.querySelector('.card div:first-child img')
+                .setAttribute('src', user.avatar_url);
+        // 프로필
+        document.getElementById('user-id').textContent = user.login;
+        document.getElementById('user-name').textContent = user.name;
+
+        });
+
+    const result2 = fetch(URL + '/users/' + `${userId}` + '/repos' ,{
+        headers: {
+            Authorization: "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"
+        },
+    });
+    result2.then(res => res.json())
+    .then(userRepos => {
+        console.log(userRepos);
+        // 저장소 이름
+        document.querySelector('.repo1 a').setAttribute('href', userRepos[0].html_url);
+        document.querySelector('.repo1-name').textContent = userRepos[0].name;
+
+        document.querySelector('.repo2 a').setAttribute('href', userRepos[1].html_url);
+        document.querySelector('.repo2-name').textContent = userRepos[1].name;
+
+        document.querySelector('.repo3 a').setAttribute('href', userRepos[2].html_url);
+        document.querySelector('.repo3-name').textContent = userRepos[2].name;
+
+    });
+    
 }
+
+
+
+// 검색버튼 이벤트
+const $search = document.querySelector('.search-img i');
+$search.addEventListener('click', e => {
+    
+    
+    renderingProfile();
+
+    let userId = document.getElementById('search').value;
+    
+    const result = fetch(URL + '/' + `${userId}`);
+    console.log(result);
+    result.then(res => res.json())
+        .then(user => {
+            console.log(user);
+            let keyName = [];
+            for(let key in user) {
+                
+                keyName.push(key)
+            }
+            console.log(keyName);
+        });
+
+});
+
+
 
 
 // 즉시실행 함수
 
 (function() {
-    mainGithub();
-    getUser();
+
 })();
