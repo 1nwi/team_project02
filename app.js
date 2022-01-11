1442f19105787ef4766b0f7bc077d5ae61ee62f7
1nwi <lllhn@naver.com>
2022-01-11 23:41:24 +0900

    [22.01.11] # project at home

--- app.js
+++ app.js
@@ -18,24 +18,27 @@ function renderingProfile() {
         },
     });
     
+
     result.then(res => res.json())
     .then(user => {
-        
-        // 사진
+            
+         // 사진
         document.querySelector('.card div:first-child img')
-                .setAttribute('src', user.avatar_url);
+                    .setAttribute('src', user.avatar_url);
         // 프로필
         document.getElementById('user-id').textContent = user.login;
         document.getElementById('user-name').textContent = user.name;
-
+        document.getElementById('followers').textContent = user.followers;
+        document.getElementById('following').textContent = user.following;
+        document.getElementById('public-repos').textContent = user.public_repos;
         });
 
-    const result2 = fetch(URL + '/users/' + `${userId}` + '/repos' ,{
-        headers: {
-            Authorization: "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"
-        },
-    });
-    result2.then(res => res.json())
+        const result2 = fetch(URL + '/users/' + `${userId}` + '/repos' ,{
+            headers: {
+                Authorization: "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"
+            },
+        });
+        result2.then(res => res.json())
     .then(userRepos => {
         console.log(userRepos);
         // 저장소 이름
@@ -49,18 +52,27 @@ function renderingProfile() {
         document.querySelector('.repo3-name').textContent = userRepos[2].name;
 
     });
-    
+
+}
+
+// 프로필카드오픈 이벤트
+function openProfileCard() {
+    document.querySelector('.card').style.display = 'flex';
 }
 
 
-
 // 검색버튼 이벤트
-const $search = document.querySelector('.search-img i');
+const $search = document.querySelector('.search-img');
 $search.addEventListener('click', e => {
     
+    const $input = document.querySelector('.search-box input');
+    if ($input.value.trim() === '') {
     
-    renderingProfile();
-
+        $input.setAttribute('placeholder', '바르게 입력하세요.');
+    } else {
+        openProfileCard();
+        renderingProfile();
+    }
     let userId = document.getElementById('search').value;
     
     const result = fetch(URL + '/' + `${userId}`);
